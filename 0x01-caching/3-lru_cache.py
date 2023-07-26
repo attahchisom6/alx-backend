1628a0089b5063fc754264e2e57725721e53d230
attahchisom6 <attahchisom6@gmail.com>
2023-07-26 12:49:41 +0100

    lru, keast accesed key is delected

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""
+A child class module of a cache system BaseCaching
+"""
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    The is an LRU model. here each time we update or add a new key to our
+    dict, we move the key to the end of a stack. if dict/cache is full, we
+    delete the least recently accessed key which is in the beginining of the
+    stack
+    Update: since the key already exists we remove it from the stack and
+    append ro the end of the stack
+    Add: since rhe key doesnt exist we append it to the end
+    """
+    def __init__(self):
+        """
+        initializing
+        """
+        super().__init__()
+        self.stack = []
+
+    def put(self, key, item):
+        """
+        this method ensures that if the key already exist, it is updated, else
+        a new item is created with rhe key
+        """
+        if key is None or item is None:
+            return
+
+        dictt = self.cache_data
+        stack = self.stack
+
+        dictt[key] = item
+
+        if key not in stack:
+            stack.append(key)
+        else:
+            stack.remove(key)
+            stack.append(key)
+
+        if len(dictt) > BaseCaching.MAX_ITEMS:
+            discarded_key = stack.pop(0)
+            print("DISCARD: {}".format(discarded_key))
+            del dictt[discarded_key]
+
+    def get(self, key):
+        """
+        get a value associated with a given key
+        """
+        dictt = self.cache_data
+
+        if key is None or key not in dictt.keys():
+            return None
+        return dictt[key]
