1c73537a25351268d454c4a871f0043533634a37
attahchisom6 <attahchisom6@gmail.com>
2023-07-26 22:13:25 +0100

    parentClass

--- 0x01-caching/base_caching.py
+++ 0x01-caching/base_caching.py
@@ -1,3 +1,35 @@
 #!/usr/bin/python3
 """
-Base Caching does the
+Base Caching does the following
+    * Base caching defines constants of your caching system where tour data are stoted
+"""
+
+
+class BaseCaching():
+    """
+    Base Caching class
+    """
+    MAX_ITEMS = 4
+
+    def __init__(self):
+        """
+        initializing
+        """
+        self.cache_data = {}
+
+    def print_cache(self):
+        """
+        print elements in the data cache
+        """
+        print("Current Cache:")
+        for key in sorted(self.cache_data.keys()):
+            print("{}: {}".format(key, self.cache_data[key]))
+
+    def put(self, key, item):
+        raise NotImplementedError("Put must be implemented in your cache class")
+
+    def get(self, key):
+        """
+        get method
+        """ 
+        raise NotImplementedError("get must be implemented in your cache class")
