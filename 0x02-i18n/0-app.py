81055582c465711599c202ed77b4e2f17e36f799
attahchisom6 <attahchisom6@gmail.com>
2023-08-01 12:09:30 +0100

    added

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,7 +8,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/", methods=["GET"], strict_slashes=False)
 def Greeting():
     """
     return a greeting string
