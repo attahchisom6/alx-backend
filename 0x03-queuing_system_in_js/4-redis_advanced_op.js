d8125c79233c8de6c2b2fdd7e3f6b036681c6cb6
attahchisom6 <attahchisom6@gmail.com>
2023-08-28 17:17:59 +0000

    add

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,38 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error}`);
+});
+
+const dict = {
+  'Portland': 50,
+  'Seattle': 80,
+  'New York': 20,
+  'Bogota': 20,
+  'Cali': 40,
+  'Paris': 2,
+};
+
+const keyValueList = Object.entries(dict);
+keyValueList.map([key, value] => {
+  client.hset('HolbertonSchools', key, value, (error, reply) => {
+    if (error) {
+      console.error(error);
+    }
+    redis.print(`Reply: ${reply}`);
+  });
+});
+
+client.hgetall('HolbertonSchools', (error, reply) => {
+  if (error) {
+    console.log(error);
+  }
+  redis.print(reply);
+  client.quit();
+});
