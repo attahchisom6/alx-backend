91a81d643cd755cbaf5dab4c84cde68a6b86fbcf
attahchisom6 <attahchisom6@gmail.com>
2023-08-29 17:14:37 +0000

    debugging

--- 0x03-queuing_system_in_js/8-job.test.js
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -1,24 +1,27 @@
 import kue from 'kue';
-import createPushNotificationJobs from './8-job':
+import createPushNotificationsJobs from './8-job':
 import { expect } from 'chai';
 
 describe('testing the createPushNotificationJobs function', () => {
+  let queue, queTest;
+
   const testQueData = {
-    message: {
+    redis: {
       host: '127.0.0.1',
       port: '6379',
-    }
-    db: 1,
+      db: 1,
+    },
   };
 
   beforeEach('setup', () => {
-    const queue = createQueue('push_notification_xode_3', testQueData);
-    queue.testMode.enter();
+    queue = createQueue(testQueData);
+    queTest = queue.testMode;
+    queue.enter();
   });
 
   afterEach('tearsown: clear enviroment after each test', () => {
-    queue.testMode.clear();
-    queue.testMode.quit();
+    queue.clear();
+    queue.exit();
   });
 
   it('Testing Error wen input data is not a list', (done) => {
@@ -27,7 +30,7 @@ describe('testing the createPushNotificationJobs function', () => {
       message: 'this is a wrong number',
     }
 
-    expect(createPushNotificationsJobs(jobs, queue)).to.throw(new Error('Jobs is not an array'));
+    expect(() => createPushNotificationsJobs(jobs, queue)).to.throw(Error, 'Jobs is not an array');
     done();
   });
 
@@ -42,6 +45,35 @@ describe('testing the createPushNotificationJobs function', () => {
         message: 'This is also a valid user',
       }
     ];
-    expect(createPushNotificationsjobs(jobs, queue)).to.equal('Notification job created: 2');
+    createPushNotificationsJobs(jobs, queue);
+    expect(queTest.jobs.length).to.equal(2);
+    done();
   });
+
+  it('Test for a completed job', (done) => {
+    const jobs = [
+      {
+        phoneNumber: '07133447722',
+        message: 'completely validated user',
+      },
+    ];
+    createPushNotificationsJobs(jobs, queue);
+    // simulate a completed job case with emit
+    queTest.jobs[0].emit('complete');
+    expect(console.log.calledWith(`Notification job ${jobs[0].id} completed`)).to.be.true;
+    done();
+  });
+
+  it('Test if our code handles appropraitely the failed occasion', (done) => {
+    const jobs = {
+      phoneNumber: '05054748698',
+      message: 'Got an error sending to u',
+    }
+    createPushNotificationsJobs(jobs, queue);
+
+    queTest.jobs[0].emit('failed', new Error('failed job'));
+    expect(console.log.callWith(`Notification job ${jobs[0].id} failed: ${error}`)).to.be.true;
+    done();
+  });
+
 });
